cbf7dbd8ebfd8967875d7820bd29c6dd280ba07a
ISAAC MAGOSA <isaacmagosa@gmail.com>
2025-10-01 01:29:51 +0300

    fix(render): robust start reads PORT in Node

---
+++ start-next.js
@@ -0,0 +1,5 @@
+﻿const { spawn } = require(" child_process\);
+const port = process.env.PORT || process.env.port || 3000;
+console.log(\PORT=\ + port);
+const child = spawn(\node_modules/.bin/next\, [\start\,\-H\,\0.0.0.0\,\-p\, String(port)], { stdio: \inherit\, shell: true });
+child.on(\exit\, code => process.exit(code));
