8a77a3f5b0ba2f3f8b77bedf3aee5235da7952ab
ISAAC MAGOSA <isaacmagosa@gmail.com>
2025-10-01 03:32:18 +0300

    chore(start): bind Render PORT via start-next.js

---
+++ start-next.js
@@ -0,0 +1,10 @@
+const { spawn } = require('child_process');
+const nextBin = require.resolve('next/dist/bin/next');
+
+const port = Number(process.env.PORT || process.env.port) || 3000;
+console.log('PORT=' + port);
+
+const child = spawn(process.execPath, [nextBin, 'start', '-H', '0.0.0.0', '-p', String(port)], {
+  stdio: 'inherit'
+});
+child.on('exit', (code) => process.exit(code));
